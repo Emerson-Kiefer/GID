bd79598dfc1f40a0b908a796bfc08d4b7fd17edd
egpickard <92379477+egpickard@users.noreply.github.com>
2023-03-23 10:15:35 -0400

    nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.png';
 import './App.css';
 import Button from '@mui/material/Button'; //importing material ui component
 import React, { useState } from "react"; //Source #4
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 const App = () => {
 /* Initial State */
@@ -15,7 +24,23 @@ const handleChange = (event) => {
 return (
 	// </>
       <div className="App">
-      <header className="App-header">
+      <nav>
+        <ul>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/events' component={Events} />
+            <Route path='/annual' component={AnnualReport} />
+            <Route path='/team' component={Teams} />
+            <Route path='/blogs' component={Blogs} />
+            <Route path='/sign-up' component={SignUp} />
+          </Routes>
+        </Router>
+        </ul>
+      </nav>
+        <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Grooming Identification Device
@@ -27,12 +52,14 @@ return (
         <p>
           Upload File  
         </p>
-          <input type="file" onChange={handleChange} />       
+          <input type="file" onChange={handleChange} />    
         </Button>
+
+
+
       </header> 
     </div>
 )
 }
 
 export default App;
-
