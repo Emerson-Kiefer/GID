95f71988679615453611b6e66e657e205c3bd1d7
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2023-03-28 18:30:15 -0400

    Added an About GID blurb

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -2,15 +2,11 @@ import React from 'react';
   
 const About = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+    <div>
+      <h1>About GID</h1>
+      <p>GID (Grooming Identification) is a web application created with the goal of assisting with the early detection of child grooming in digital communications. </p>
+      <p>GID uses a modified version of the <a href="https://github.com/danielafe7-usp/BF-PSR-Framework" target="_blank">BF-PSR Framework</a>, a state of the art grooming classification model.</p>
+      <p>For more information about the creation and performance of the BF-PSR framework, read the <a href="https://www.sciencedirect.com/science/article/abs/pii/S0950705121011187">study</a> th </p>
     </div>
   );
 };
