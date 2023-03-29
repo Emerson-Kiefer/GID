242403b22ed870eb8d2519b45ce8b0438e0f2425
egpickard <92379477+egpickard@users.noreply.github.com>
2023-03-29 15:06:46 -0400

    changed background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,4 @@
-// import logo from "./logo.png";
-// // import "./App.css";
-// import Button from "@mui/material/Button"; //importing material ui component
-import React, { useState } from "react"; //Source #4
+import React from "react"; //Source #4
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
@@ -10,16 +7,8 @@ import Events from "./pages/events";
 import AnnualReport from "./pages/annual";
 import Teams from "./pages/team";
 import Blogs from "./pages/blogs";
-import SignUp from "./pages/signup";
 
 const App = () => {
-  /* Initial State */
-  // let [Name, setname] = useState("");
-
-  // /* The handleChange() function to set a new state for input */
-  // const handleChange = (event) => {
-  //   setname(event.target.value);
-  // };
   return (
     // </>
     <nav className="navbar">
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,5 @@
-// import React from "react";
 import logo from "./logo.png";
 import "./App.css";
-import Button from "@mui/material/Button"; //importing material ui component
 import React, { useState } from "react"; //Source #4
 import FileUploadField from "../components/fileUploadField";
 import PredictButton from "./predict_button";
@@ -15,7 +13,11 @@ const Home = () => {
         <p>
           <img src={logo} className="App-logo" alt="logo" />
           <p>Grooming Identification Device</p>
-          <FileUploadField files={files} setFiles={setFiles} />
+          <FileUploadField
+            files={files}
+            setFiles={setFiles}
+            className="file-upload"
+          />
         </p>
       </header>
       <PredictButton></PredictButton>
