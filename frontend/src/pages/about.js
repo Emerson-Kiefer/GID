36768b5a8984792be037dc96fa43346b9d342e9e
egpickard <92379477+egpickard@users.noreply.github.com>
2023-03-29 15:09:29 -0400

    changed text color on about page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,14 +1,37 @@
-import React from 'react';
-  
+import React from "react";
+import "./App.css";
+
 const About = () => {
   return (
-    <div>
-      <h1>About GID</h1>
-      <p>GID (Grooming Identification) is a web application created with the goal of assisting with the early detection of child grooming in digital communications. </p>
-      <p>GID uses a modified version of the <a href="https://github.com/danielafe7-usp/BF-PSR-Framework" target="_blank">BF-PSR Framework</a>, a state of the art grooming classification model.</p>
-      <p>For more information about the creation and performance of the BF-PSR framework, read the <a href="https://www.sciencedirect.com/science/article/abs/pii/S0950705121011187">study</a> th </p>
+    <div className="App">
+      <div>
+        <h1>About GID</h1>
+        <p>
+          GID (Grooming Identification) is a web application created with the
+          goal of assisting with the early detection of child grooming in
+          digital communications.{" "}
+        </p>
+        <p>
+          GID uses a modified version of the{" "}
+          <a
+            href="https://github.com/danielafe7-usp/BF-PSR-Framework"
+            target="_blank"
+          >
+            BF-PSR Framework
+          </a>
+          , a state of the art grooming classification model.
+        </p>
+        <p>
+          For more information about the creation and performance of the BF-PSR
+          framework, read the{" "}
+          <a href="https://www.sciencedirect.com/science/article/abs/pii/S0950705121011187">
+            study
+          </a>{" "}
+          th{" "}
+        </p>
+      </div>
     </div>
   );
 };
-  
+
 export default About;
